efb709840da6fdac0305287241b1cd098ab16894
vaTheo <vincenta.theo@gmail.com>
2024-12-18 16:38:54 +0100

    shadcn installaed

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,17 +1,13 @@
 // packages/frontend/src/App.tsx
 import { useState } from 'react';
+import { Button } from './components/ui/button';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-      <div>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Count is {count}
-        </button>
-      </div>
+      <Button>Click me pls !</Button>
     </div>
   );
 }
